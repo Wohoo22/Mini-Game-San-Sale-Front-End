ee7bcf803b7fbea59e2850c41899b452cd30bbfc
quandang2002 <quanvda>
2021-07-19 01:39:00 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 App({
   onLaunch(options) {
-    my.navigateTo({
-      url:"pages/created_campaign/created_campaign"
-    });
+    // my.navigateTo({
+    //   url:"pages/created_campaign/created_campaign"
+    // });
   },
   onShow(options) {
   },
