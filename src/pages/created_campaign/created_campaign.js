00e636d8fb734694f1ebdaf16fa601ede0b1fbab
quandang2002 <quanvda>
2021-07-20 20:36:59 +0700

    update

--- src/pages/created_campaign/created_campaign.js
+++ src/pages/created_campaign/created_campaign.js
@@ -1,1 +1,29 @@
-Page({})
+var app = getApp();
+
+function getData() {
+  // fake data
+  app.createdCampaignData.useCnt = 150;
+  app.createdCampaignData.winCnt = 100;
+  app.createdCampaignData.loseCnt = 50;
+  app.createdCampaignData.miniGames = [];
+  app.createdCampaignData.miniGames.push({
+    id: '1',
+    name: 'Snake',
+    winPrizes: ['GIAMGIA10k','FREESHIP100k'],
+    losePrizes: ['GIAMGIA5K','FREESHIP500l'],
+  });
+
+  // call api
+}
+
+Page({
+  data: {
+    useCnt: app.createdCampaignData.useCnt,
+    winCnt: app.createdCampaignData.winCnt,
+    loseCnt: app.createdCampaignData.loseCnt,
+    miniGames: app.createdCampaignData.miniGames
+  },
+  onLoad() {
+    getData();
+  }
+})
