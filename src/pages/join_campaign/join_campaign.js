d4d75465a7f9d278084ff6050f5746ebb036f7ac
quandang2002 <quanvda>
2021-07-23 00:14:25 +0700

    update

--- src/pages/join_campaign/join_campaign.js
+++ src/pages/join_campaign/join_campaign.js
@@ -17,44 +17,103 @@ var processedData = {
   }]
 };
 
-function getData() {
+async function getData() {
+
   // fake data
-  processedData = {
-    id: '19n3i1yhd03',
-    name: 'Săn sale tháng 7',
-    expireAt: '30/7/2021',
-    playCnt: 145,
-    winCnt: 100,
-    loseCnt: 45,
-    miniGames: [{
-      id: '1234242342',
-      uuid: '19327342383',
-      name: 'Snake',
-      thumbnail: 'https://cdn.tutsplus.com/mobile/uploads/legacy/Corona-SDK_Build-A-Snake-Game/2/1.png',
-      likeCnt: 1293,
-      dislikeCnt: 42,
-      categories: ['Hanh Dong', 'Phieu Luu']
-    },
-    {
-      id: '1234242342',
-      uuid: '19327342383',
-      name: 'Mario',
-      thumbnail: 'https://pbs.twimg.com/media/Edlq-09UEAAbOVy.jpg',
-      likeCnt: 1293,
-      dislikeCnt: 42,
-      categories: ['Hanh Dong', 'Phieu Luu']
-    }]
-  }
+
+  // processedData = {
+  //   id: '19n3i1yhd03',
+  //   name: 'Săn sale tháng 7',
+  //   expireAt: '30/7/2021',
+  //   playCnt: 145,
+  //   winCnt: 100,
+  //   loseCnt: 45,
+  //   miniGames: [{
+  //     id: '1234242342',
+  //     uuid: '19327342383',
+  //     name: 'Snake',
+  //     thumbnail: 'https://cdn.tutsplus.com/mobile/uploads/legacy/Corona-SDK_Build-A-Snake-Game/2/1.png',
+  //     likeCnt: 1293,
+  //     dislikeCnt: 42,
+  //     categories: ['Hanh Dong', 'Phieu Luu']
+  //   },
+  //   {
+  //     id: '1234242342',
+  //     uuid: '19327342383',
+  //     name: 'Mario',
+  //     thumbnail: 'https://pbs.twimg.com/media/Edlq-09UEAAbOVy.jpg',
+  //     likeCnt: 1293,
+  //     dislikeCnt: 42,
+  //     categories: ['Hanh Dong', 'Phieu Luu']
+  //   }]
+  // }
+
+
   // call api
+
   var campaignId = app.indexData.joinCampaignId;
+  var campaignData = await app.httpPost(
+  app.globalData.server + '/campaign/get',
+    {
+      ids: [campaignId],
+    }
+  );
+
+  if (campaignData.length == 0) {
+    my.navigateBack();
+    return;
+  }
+
+  campaignData = campaignData[0];
+
+  processedData = {
+    id: campaignData.id,
+    name: campaignData.name,
+    expireAt: campaignData.expireAt,
+    playCnt: campaignData.playCount,
+    winCnt: campaignData.winCount,
+    loseCnt: campaignData.loseCount,
+    miniGames: []
+  };
+
+  var miniGameIds = [];
+  for (var g of campaignData.miniGames) {
+    miniGameIds.push(g.id);
+  }
+
+  var gameInfo = await app.httpPost(
+  app.globalData.server + '/minigame/get',
+    {
+      ids: miniGameIds,
+    }
+  );
+
+  for (var g of gameInfo) {
+    var uuid = '';
+    for (var mg of campaignData.miniGames) {
+      if (mg.id == g.id) {
+        uuid = mg.id;
+        break;
+      }
+    }
+    processedData.miniGames.push({
+        id: g.id,
+        uuid: uuid,
+        name: g.name,
+        thumbnail: g.thumbnail,
+        likeCnt: g.likeCount,
+        dislikeCnt: g.dislikeCount,
+        categories: g.categoryIds
+    })
+  }
 }
 
 Page({
   data: {
     processedData: processedData,
   },
-  onLoad() {
-    getData();
+ async onLoad() {
+    await getData();
     this.setData({
       processedData: processedData,
     })
